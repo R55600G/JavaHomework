d434ad061d528c98ad2d1d1d149430c1de117aa2
R55600G <lbw13569601677@163.com>
2026-03-24 18:12:12 +0800

    first

---
+++ src/cn/henu/software/stu2510250128/ch01/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.henu.software.stu2510250128.ch01;
+
+/**
+ * 这是第一章的第一个 Java 程序
+ * 主要是为了测试开发环境是否配置正确
+ */
+public class HelloWorld {
+
+    // main 方法是整个 Java 程序的入口点，程序从这里开始执行
+    public static void main(String[] args) {
+        // 这行代码的作用是在控制台输出引号里面的文字
+        System.out.println("Hello, World! 河南大学软件工程系，你好！");
+    }
+}
