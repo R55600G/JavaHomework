53fb49be838e5f73545f6efc536e5e82d44661c3
R55600G <lbw13569601677@163.com>
2026-04-28 21:35:11 +0800

    4.28

---
+++ src/cn/henu/software/stu2510250128/ch03/S3_1.java
@@ -0,0 +1,10 @@
+package cn.henu.software.stu2510250128.ch03;
+class Book{
+    String bookNo;
+    String bookTitle;
+    String author;
+    int yearPublished;
+    public Book(String bookNo,String bookTitle,String author,int yearPublished){
+
+    }
+        }
